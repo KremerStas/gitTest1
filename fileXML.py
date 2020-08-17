062bee9333b89775147361af8e223feb2b79c0c6
KremerStas <red-bull1951@yandex.ru>
2020-08-17 20:43:22 +0300

    second

--- fileXML.py
+++ fileXML.py
@@ -1,10 +1,11 @@
 from xml.etree import ElementTree
+
 tree = ElementTree.parse('example1.xml')
 root = tree.getroot()
 # print(root)
 # print(root.tag, root.attrib)
 # for child in root:
 #     print(child.tag, child.attrib)
-# for element in root.iter('name'):
-#     print()#не могу перебрать элементы name1,name2,name3
+for element in root.iter('name'):
+    print(element)  # не могу перебрать элементы name1,name2,name3
 tree.write('example1_new.xml')
