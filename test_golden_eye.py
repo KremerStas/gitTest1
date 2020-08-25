76f5664e4d6b706d63bfe37ddac549935a6119c3
KremerStas <red-bull1951@yandex.ru>
2020-08-25 21:08:57 +0300

    1

---
+++ test_golden_eye.py
@@ -0,0 +1,32 @@
+import sqlite3
+
+conn = sqlite3.connect('golden-eye.db')
+cur = conn.cursor()
+# cur.execute("SELECT * FROM xrates")
+rows = cur.fetchall()
+for row in rows:
+    print(row)
+
+conn.close()
+
+from peewee import (SqliteDatabase, Model, IntegerField, DoubleField, DateTimeField, datetime as peewee_datetime)
+
+db = SqliteDatabase('golden-eye.db')
+
+
+# class XRate(Model):
+#     class Meta:
+#         database = db
+#         db_table = 'xrates'
+#         index = (
+#             (('from_currency', 'to_currency'), True),
+#         )
+#
+#     from_currency = IntegerField()
+#     to_currency = IntegerField()
+#     rate = DoubleField()
+#     update = DateTimeField(default=peewee_datetime.datetime.now)
+import models
+
+models.XRate.create_table()
+models.XRate.create(from_currency=980, to_currency=840, rate=25)
